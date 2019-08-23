de8fa147a890c7b1b6bbcd5fe002bfdf9ddfbdee
hupe1980 <hello@webappsolutions.de>
2019-08-23 22:56:11 +0200

    chore: Refactor

--- packages/cdk-pull-request-check/src/__tests__/pull-request-check.test.ts
+++ packages/cdk-pull-request-check/src/__tests__/pull-request-check.test.ts
@@ -1,24 +1,50 @@
-import { Stack } from '@aws-cdk/core';
+import {
+    BuildSpec,
+    ComputeType,
+    LinuxBuildImage,
+} from '@aws-cdk/aws-codebuild';
 import { Repository } from '@aws-cdk/aws-codecommit';
-import { BuildSpec } from '@aws-cdk/aws-codebuild';
+import { Stack } from '@aws-cdk/core';
 import 'jest-cdk-snapshot';
 
 import { PullRequestCheck } from '../pull_request_check';
 
-describe('cdk-pull-request-check', (): void => {
-    it('snapshot', (): void => {
-        const stack = new Stack();
+test('default setup', (): void => {
+    // GIVEN
+    const stack = new Stack();
 
-        const repository = new Repository(stack, 'Repository', {
-            repositoryName: 'MyRepositoryName',
-            description: 'Some description.', // optional property
-        });
-
-        new PullRequestCheck(stack, 'PullRequestCheck', {
-            repository,
-            buildSpec: BuildSpec.fromSourceFilename('buildspecs/prcheck.yml'),
-        });
-
-        expect(stack).toMatchCdkSnapshot();
+    const repository = new Repository(stack, 'Repository', {
+        repositoryName: 'MyRepositoryName',
+        description: 'Some description.',
     });
+
+    // WHEN
+    new PullRequestCheck(stack, 'PullRequestCheck', {
+        repository,
+        buildSpec: BuildSpec.fromSourceFilename('buildspecs/prcheck.yml'),
+    });
+
+    // THEN
+    expect(stack).toMatchCdkSnapshot();
+});
+
+test('custom setup', (): void => {
+    // GIVEN
+    const stack = new Stack();
+
+    const repository = new Repository(stack, 'Repository', {
+        repositoryName: 'MyRepositoryName',
+        description: 'Some description.',
+    });
+
+    // WHEN
+    new PullRequestCheck(stack, 'PullRequestCheck', {
+        repository,
+        buildSpec: BuildSpec.fromSourceFilename('buildspecs/prcheck.yml'),
+        computeType: ComputeType.LARGE,
+        buildImage: LinuxBuildImage.UBUNTU_14_04_PYTHON_3_7_1,
+    });
+
+    // THEN
+    expect(stack).toMatchCdkSnapshot();
 });
--- packages/cdk-pull-request-check/src/pull_request_check.ts
+++ packages/cdk-pull-request-check/src/pull_request_check.ts
@@ -1,40 +1,65 @@
-import * as path from 'path';
-import { Construct } from '@aws-cdk/core';
-import { IRepository } from '@aws-cdk/aws-codecommit';
-import { Function, Runtime, Code } from '@aws-cdk/aws-lambda';
-import { ServicePrincipal, Role, PolicyStatement } from '@aws-cdk/aws-iam';
 import {
+    BuildSpec,
+    ComputeType,
+    IBuildImage,
+    LinuxBuildImage,
     Project,
     Source,
-    LinuxBuildImage,
-    ComputeType,
-    BuildSpec,
 } from '@aws-cdk/aws-codebuild';
-import { LambdaFunction } from '@aws-cdk/aws-events-targets';
+import { IRepository } from '@aws-cdk/aws-codecommit';
+import { EventField, RuleTargetInput } from '@aws-cdk/aws-events';
+import { CodeBuildProject, LambdaFunction } from '@aws-cdk/aws-events-targets';
+import { PolicyStatement, Role, ServicePrincipal } from '@aws-cdk/aws-iam';
+import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
+import { Construct } from '@aws-cdk/core';
+import * as path from 'path';
 
-import { PullRequestProjectTarget } from './pull_request_project_target';
-
-const pathLambdas = path.join(__dirname, '..', 'lambdas');
+const lambdaPath = path.join(__dirname, '..', 'lambdas');
 
 export interface PullRequestCheckProps {
-    repository: IRepository;
-    buildSpec: BuildSpec;
-    buildImage?: LinuxBuildImage;
-    computeType?: ComputeType;
+    /**
+     * The CodeCommit repository.
+     */
+    readonly repository: IRepository;
+
+    /**
+     * Filename or contents of buildspec in JSON format.
+     * @see https://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html#build-spec-ref-example
+     */
+    readonly buildSpec: BuildSpec;
+
+    /**
+     * Build environment to use for the build.
+     *
+     * @default BuildEnvironment.LinuxBuildImage.STANDARD_2_0
+     */
+    readonly buildImage?: IBuildImage;
+
+    /**
+     * The type of compute to use for this build.
+     * See the {@link ComputeType} enum for the possible values.
+     *
+     * @default taken from {@link #buildImage#defaultComputeType}
+     */
+    readonly computeType?: ComputeType;
 }
+
+/**
+ * Represents a reference to a PullRequestCheck.
+ */
 export class PullRequestCheck extends Construct {
     public constructor(
-        parent: Construct,
+        scope: Construct,
         id: string,
         props: PullRequestCheckProps,
     ) {
-        super(parent, id);
+        super(scope, id);
 
         const {
             repository,
             buildSpec,
             buildImage = LinuxBuildImage.STANDARD_2_0,
-            computeType = ComputeType.SMALL,
+            computeType = buildImage.defaultComputeType,
         } = props;
 
         const lambdaRole = new Role(this, 'LambdaRole', {
@@ -57,7 +82,7 @@ export class PullRequestCheck extends Construct {
 
         const pullRequestFunction = new Function(this, 'PullRequestFunction', {
             runtime: Runtime.PYTHON_3_7,
-            code: Code.asset(`${pathLambdas}/pull-request`),
+            code: Code.asset(`${lambdaPath}/pull-request`),
             handler: 'pull_request.lambda_handler',
             role: lambdaRole,
         });
@@ -67,7 +92,7 @@ export class PullRequestCheck extends Construct {
             'CodeBuildResultFunction',
             {
                 runtime: Runtime.PYTHON_3_7,
-                code: Code.asset(`${pathLambdas}/code-build-result`),
+                code: Code.asset(`${lambdaPath}/code-build-result`),
                 handler: 'code_build_result.lambda_handler',
                 role: lambdaRole,
             },
@@ -104,6 +129,41 @@ export class PullRequestCheck extends Construct {
         );
 
         rule.addTarget(new LambdaFunction(pullRequestFunction));
-        rule.addTarget(new PullRequestProjectTarget(pullRequestProject));
+        rule.addTarget(
+            new CodeBuildProject(pullRequestProject, {
+                event: RuleTargetInput.fromObject({
+                    sourceVersion: EventField.fromPath('$.detail.sourceCommit'),
+                    artifactsOverride: { type: 'NO_ARTIFACTS' },
+                    environmentVariablesOverride: [
+                        {
+                            name: 'pullRequestId',
+                            value: EventField.fromPath(
+                                '$.detail.pullRequestId',
+                            ),
+                            type: 'PLAINTEXT',
+                        },
+                        {
+                            name: 'repositoryName',
+                            value: EventField.fromPath(
+                                '$.detail.repositoryNames[0]',
+                            ),
+                            type: 'PLAINTEXT',
+                        },
+                        {
+                            name: 'sourceCommit',
+                            value: EventField.fromPath('$.detail.sourceCommit'),
+                            type: 'PLAINTEXT',
+                        },
+                        {
+                            name: 'destinationCommit',
+                            value: EventField.fromPath(
+                                '$.detail.destinationCommit',
+                            ),
+                            type: 'PLAINTEXT',
+                        },
+                    ],
+                }),
+            }),
+        );
     }
 }
