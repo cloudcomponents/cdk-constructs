f9b90b1935d6cd0299935ff6f472ba0cbdb4b8c0
Carsten Thiele <software@carsten-thiele.de>
2019-12-15 11:08:24 +0100

    feat: slack-approval: show AWS Region in slack approval Dialog

--- packages/cdk-codepipeline-slack-lambda/src/approval-requester.ts
+++ packages/cdk-codepipeline-slack-lambda/src/approval-requester.ts
@@ -2,8 +2,9 @@ import { SNSEvent } from 'aws-lambda';
 import { requestApproval } from './interactions/approval-interactions';
 
 export const handler = async (event: SNSEvent): Promise<void> => {
-    const region = event.Records[0].EventSubscriptionArn.split(':')[3];
-    const message = JSON.parse(event.Records[0].Sns.Message as string);
-    const approval = { region, ...message };
+    const region = process.env.AWS_REGION;
+    const message = event.Records[0].Sns.Message as string;
+    const { approval } = JSON.parse(message);
+    approval.region = region;
     return requestApproval(approval);
 };
