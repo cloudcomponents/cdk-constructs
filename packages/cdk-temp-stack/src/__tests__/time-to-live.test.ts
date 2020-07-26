30e7abcf9e30c06d203d97d23c93a31fa852b6df
hupe1980 <frankhuebner1980@gmail.com>
2020-07-26 10:42:00 +0200

    chore: Add  unittests

--- packages/cdk-temp-stack/src/__tests__/time-to-live.test.ts
+++ packages/cdk-temp-stack/src/__tests__/time-to-live.test.ts
@@ -1,6 +1,8 @@
-import { Duration, Stack } from '@aws-cdk/core';
+import { App, Duration, Stack } from '@aws-cdk/core';
+import { SynthUtils } from '@aws-cdk/assert';
 import 'jest-cdk-snapshot';
 
+import { TempStack } from '../temp-stack';
 import { TimeToLive } from '../time-to-live';
 
 test('default setup', () => {
@@ -14,4 +16,38 @@ test('default setup', () => {
 
   // THEN
   expect(stack).toMatchCdkSnapshot();
+});
+
+test('multiple instances', () => {
+  // GIVEN
+  const stack = new Stack();
+
+  // WHEN
+  new TimeToLive(stack, 'TimeToLive1', {
+    ttl: Duration.minutes(10),
+  });
+
+  new TimeToLive(stack, 'TimeToLive2', {
+    ttl: Duration.minutes(10),
+  });
+
+  // THEN
+  expect(() => SynthUtils.toCloudFormation(stack)).toThrowError();
+});
+
+test('one instance + temp stack', () => {
+  // GIVEN
+  const app = new App();
+
+  // WHEN
+  const stack = new TempStack(app, 'TestStack', {
+    ttl: Duration.minutes(10),
+  });
+
+  new TimeToLive(stack, 'TimeToLive2', {
+    ttl: Duration.minutes(10),
+  });
+
+  // THEN
+  expect(() => SynthUtils.toCloudFormation(stack)).toThrowError();
 });
