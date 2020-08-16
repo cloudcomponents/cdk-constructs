0d3842f2691a969074f7ff939999f3e402c36e9f
hupe1980 <frankhuebner1980@gmail.com>
2020-08-16 15:49:05 +0200

    chore: Update denpendencies

--- packages/cdk-cloudfront-authorization/src/distributions.ts
+++ packages/cdk-cloudfront-authorization/src/distributions.ts
@@ -1,5 +1,5 @@
 import { ICertificate } from '@aws-cdk/aws-certificatemanager';
-import { IOrigin, Distribution, ErrorResponse, PriceClass, CfnDistribution } from '@aws-cdk/aws-cloudfront';
+import { IOrigin, Distribution, ErrorResponse, PriceClass, HttpVersion } from '@aws-cdk/aws-cloudfront';
 import { S3Origin } from '@aws-cdk/aws-cloudfront-origins';
 import { Construct, Duration, RemovalPolicy } from '@aws-cdk/core';
 import { DeletableBucket } from '@cloudcomponents/cdk-deletable-bucket';
@@ -7,9 +7,83 @@ import { DeletableBucket } from '@cloudcomponents/cdk-deletable-bucket';
 import { IAuthorization, IStaticSiteAuthorization, ISpaAuthorization } from './authorizations';
 
 export interface CommonDistributionProps {
+  /**
+   * The origin that you want CloudFront to route requests
+   */
   readonly origin?: IOrigin;
+  /**
+   * The price class that corresponds with the maximum price that you want to pay for CloudFront service.
+   * If you specify PriceClass_All, CloudFront responds to requests for your objects from all CloudFront edge locations.
+   * If you specify a price class other than PriceClass_All, CloudFront serves your objects from the CloudFront edge location
+   * that has the lowest latency among the edge locations in your price class.
+   *
+   * @default PriceClass.PRICE_CLASS_100
+   */
   readonly priceClass?: PriceClass;
+
+  /**
+   * A certificate to associate with the distribution. The certificate must be located in N. Virginia (us-east-1).
+   *
+   * @default - the CloudFront wildcard certificate (*.cloudfront.net) will be used.
+   */
   readonly certificate?: ICertificate;
+
+  /**
+   * Any comments you want to include about the distribution.
+   *
+   * @default - no comment
+   */
+  readonly comment?: string;
+
+  /**
+   * The object that you want CloudFront to request from your origin (for example, index.html)
+   * when a viewer requests the root URL for your distribution. If no default object is set, the
+   * request goes to the origin's root (e.g., example.com/).
+   *
+   * @default - index.html
+   */
+  readonly defaultRootObject?: string;
+
+  /**
+   * Enable or disable the distribution.
+   *
+   * @default true
+   */
+  readonly enabled?: boolean;
+
+  /**
+   * Whether CloudFront will respond to IPv6 DNS requests with an IPv6 address.
+   *
+   * If you specify false, CloudFront responds to IPv6 DNS requests with the DNS response code NOERROR and with no IP addresses.
+   * This allows viewers to submit a second request, for an IPv4 address for your distribution.
+   *
+   * @default true
+   */
+  readonly enableIpv6?: boolean;
+
+  /**
+   * Specify the maximum HTTP version that you want viewers to use to communicate with CloudFront.
+   *
+   * For viewers and CloudFront to use HTTP/2, viewers must support TLS 1.2 or later, and must support server name identification (SNI).
+   *
+   * @default HttpVersion.HTTP2
+   */
+  readonly httpVersion?: HttpVersion;
+
+  /**
+   * Unique identifier that specifies the AWS WAF web ACL to associate with this CloudFront distribution.
+   *
+   * To specify a web ACL created using the latest version of AWS WAF, use the ACL ARN, for example
+   * `arn:aws:wafv2:us-east-1:123456789012:global/webacl/ExampleWebACL/473e64fd-f30b-4765-81a0-62ad96dd167a`.
+   * To specify a web ACL created using AWS WAF Classic, use the ACL ID, for example `473e64fd-f30b-4765-81a0-62ad96dd167a`.
+   *
+   * @see https://docs.aws.amazon.com/waf/latest/developerguide/what-is-aws-waf.html
+   * @see https://docs.aws.amazon.com/cloudfront/latest/APIReference/API_CreateDistribution.html#API_CreateDistribution_RequestParameters.
+   *
+   * @default - No AWS Web Application Firewall web access control list (web ACL).
+   */
+  readonly webAclId?: string;
+
   readonly removalPolicy?: RemovalPolicy;
 }
 
@@ -27,15 +101,18 @@ export class BaseDistribution extends Construct {
     const origin = props.origin ?? this.defaultOrigin(removalPolicy === RemovalPolicy.DESTROY);
 
     const distribution = new Distribution(this, 'Distribution', {
+      enabled: props.enabled ?? true,
+      enableIpv6: props.enableIpv6 ?? true,
+      comment: props.comment,
       priceClass: props.priceClass ?? PriceClass.PRICE_CLASS_100,
+      httpVersion: props.httpVersion ?? HttpVersion.HTTP2,
+      webAclId: props.webAclId,
       errorResponses: props.errorResponses,
       certificate: props.certificate,
       defaultBehavior: props.authorization.createDefaultBehavior(origin),
       additionalBehaviors: props.authorization.createAdditionalBehaviors(origin),
+      defaultRootObject: props.defaultRootObject ?? 'index.html',
     });
-
-    //TODO
-    (distribution.node.findChild('CFDistribution') as CfnDistribution).addOverride('Properties.DistributionConfig.DefaultRootObject', 'index.html');
 
     props.authorization.updateUserPoolClientCallbacks({
       callbackUrls: [`https://${distribution.distributionDomainName}${props.authorization.redirectPaths.signIn}`],
