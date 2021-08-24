b3a05a96131de884b466dec57adea0ec14223328
kimisme9386 <kimisme9386@gmail.com>
2021-08-24 14:25:02 +0800

    fix: props syntax

--- packages/cdk-blue-green-container-deployment/src/ecs-deployment-configuration.ts
+++ packages/cdk-blue-green-container-deployment/src/ecs-deployment-configuration.ts
@@ -1,8 +1,30 @@
-import { IEcsDeploymentConfig, CfnDeploymentConfigProps, CfnDeploymentConfig } from '@aws-cdk/aws-codedeploy';
+import { IEcsDeploymentConfig, CfnDeploymentConfig } from '@aws-cdk/aws-codedeploy';
 
-import { Aws, Construct } from '@aws-cdk/core';
+import { Aws, Construct, IResolvable } from '@aws-cdk/core';
 
-export type EcsDeploymentConfigurationProps = Omit<CfnDeploymentConfigProps, 'computePlatform'>;
+export interface EcsDeploymentConfigurationProps {
+  /**
+   * `AWS::CodeDeploy::DeploymentConfig.DeploymentConfigName`.
+   *
+   * @external
+   * @link http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-codedeploy-deploymentconfig.html#cfn-codedeploy-deploymentconfig-deploymentconfigname
+   */
+  readonly deploymentConfigName?: string;
+  /**
+   * `AWS::CodeDeploy::DeploymentConfig.MinimumHealthyHosts`.
+   *
+   * @external
+   * @link http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-codedeploy-deploymentconfig.html#cfn-codedeploy-deploymentconfig-minimumhealthyhosts
+   */
+  readonly minimumHealthyHosts?: CfnDeploymentConfig.MinimumHealthyHostsProperty | IResolvable;
+  /**
+   * `AWS::CodeDeploy::DeploymentConfig.TrafficRoutingConfig`.
+   *
+   * @external
+   * @link http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-codedeploy-deploymentconfig.html#cfn-codedeploy-deploymentconfig-trafficroutingconfig
+   */
+  readonly trafficRoutingConfig?: CfnDeploymentConfig.TrafficRoutingConfigProperty | IResolvable;
+}
 
 export class EcsDeploymentConfiguration extends Construct {
   readonly deploymentConfig: IEcsDeploymentConfig;
