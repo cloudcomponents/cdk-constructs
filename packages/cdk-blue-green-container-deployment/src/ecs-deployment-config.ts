2f569e2d289c421e11d211c819c7335339c77db6
Eric Ellingson <god@eric.wtf>
2022-01-09 15:19:47 -0600

    chore: use v2 for EcsDeploymentConfig

--- packages/cdk-blue-green-container-deployment/src/ecs-deployment-config.ts
+++ packages/cdk-blue-green-container-deployment/src/ecs-deployment-config.ts
@@ -1,5 +1,6 @@
-import { CfnDeploymentConfig } from '@aws-cdk/aws-codedeploy';
-import { Aws, Construct, IResolvable, Resource } from '@aws-cdk/core';
+import { CfnDeploymentConfig } from "aws-cdk-lib/aws-codedeploy";
+import { Aws, IResolvable, Resource } from "aws-cdk-lib/core";
+import { Construct } from "constructs";
 
 export interface IEcsDeploymentConfig {
   readonly deploymentConfigName: string;
@@ -20,22 +21,39 @@ export interface EcsDeploymentConfigurationProps {
    * @external
    * @link http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-codedeploy-deploymentconfig.html#cfn-codedeploy-deploymentconfig-minimumhealthyhosts
    */
-  readonly minimumHealthyHosts?: CfnDeploymentConfig.MinimumHealthyHostsProperty | IResolvable;
+  readonly minimumHealthyHosts?:
+    | CfnDeploymentConfig.MinimumHealthyHostsProperty
+    | IResolvable;
   /**
    * `AWS::CodeDeploy::DeploymentConfig.TrafficRoutingConfig`.
    *
    * @external
    * @link http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-codedeploy-deploymentconfig.html#cfn-codedeploy-deploymentconfig-trafficroutingconfig
    */
-  readonly trafficRoutingConfig?: CfnDeploymentConfig.TrafficRoutingConfigProperty | IResolvable;
+  readonly trafficRoutingConfig?:
+    | CfnDeploymentConfig.TrafficRoutingConfigProperty
+    | IResolvable;
 }
 
-export class EcsDeploymentConfig extends Resource implements IEcsDeploymentConfig {
-  public static readonly LINEAR_10PERCENT_EVERY_1MINUTE = deploymentConfig('CodeDeployDefault.ECSLinear10PercentEvery1Minutes');
-  public static readonly LINEAR_10PERCENT_EVERY_3MINUTES = deploymentConfig('CodeDeployDefault.ECSLinear10PercentEvery3Minutes');
-  public static readonly CANARY_10PERCENT_5MINUTES = deploymentConfig('CodeDeployDefault.ECSCanary10Percent5Minutes');
-  public static readonly CANARY_10PERCENT_15MINUTES = deploymentConfig('CodeDeployDefault.ECSCanary10Percent15Minutes');
-  public static readonly ALL_AT_ONCE = deploymentConfig('CodeDeployDefault.ECSAllAtOnce');
+export class EcsDeploymentConfig
+  extends Resource
+  implements IEcsDeploymentConfig
+{
+  public static readonly LINEAR_10PERCENT_EVERY_1MINUTE = deploymentConfig(
+    "CodeDeployDefault.ECSLinear10PercentEvery1Minutes"
+  );
+  public static readonly LINEAR_10PERCENT_EVERY_3MINUTES = deploymentConfig(
+    "CodeDeployDefault.ECSLinear10PercentEvery3Minutes"
+  );
+  public static readonly CANARY_10PERCENT_5MINUTES = deploymentConfig(
+    "CodeDeployDefault.ECSCanary10Percent5Minutes"
+  );
+  public static readonly CANARY_10PERCENT_15MINUTES = deploymentConfig(
+    "CodeDeployDefault.ECSCanary10Percent15Minutes"
+  );
+  public static readonly ALL_AT_ONCE = deploymentConfig(
+    "CodeDeployDefault.ECSAllAtOnce"
+  );
 
   /**
    * Import a custom Deployment Configuration for an ECS Deployment Group defined outside the CDK.
@@ -45,23 +63,37 @@ export class EcsDeploymentConfig extends Resource implements IEcsDeploymentConfi
    * @param ecsDeploymentConfigName the name of the referenced custom Deployment Configuration
    * @returns a Construct representing a reference to an existing custom Deployment Configuration
    */
-  public static fromEcsDeploymentConfigName(_scope: Construct, _id: string, ecsDeploymentConfigName: string): IEcsDeploymentConfig {
+  public static fromEcsDeploymentConfigName(
+    _scope: Construct,
+    _id: string,
+    ecsDeploymentConfigName: string
+  ): IEcsDeploymentConfig {
     return deploymentConfig(ecsDeploymentConfigName);
   }
 
   public readonly deploymentConfigName: string;
   public readonly deploymentConfigArn: string;
 
-  constructor(scope: Construct, id: string, props: EcsDeploymentConfigurationProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: EcsDeploymentConfigurationProps
+  ) {
     super(scope, id);
 
-    const cfnDeploymentConfig = new CfnDeploymentConfig(this, 'EcsDeploymentConfiguration', {
-      computePlatform: 'ECS',
-      ...props,
-    });
+    const cfnDeploymentConfig = new CfnDeploymentConfig(
+      this,
+      "EcsDeploymentConfiguration",
+      {
+        computePlatform: "ECS",
+        ...props,
+      }
+    );
 
     this.deploymentConfigName = cfnDeploymentConfig.ref;
-    this.deploymentConfigArn = arnForDeploymentConfig(this.deploymentConfigName);
+    this.deploymentConfigArn = arnForDeploymentConfig(
+      this.deploymentConfigName
+    );
   }
 }
 
