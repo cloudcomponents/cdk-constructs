301d16ceb3ae21e9bb7b546a358b886d3838c3e1
Jeff Cooper <jeff@cuperman.net>
2022-03-13 12:15:09 -0700

    Switch back to using @aws-cdk/assert

--- packages/cdk-blue-green-container-deployment/src/__tests__/ecs-service.test.ts
+++ packages/cdk-blue-green-container-deployment/src/__tests__/ecs-service.test.ts
@@ -1,7 +1,5 @@
-/* eslint jest/expect-expect: "off" */
-
+import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
@@ -9,8 +7,9 @@ import { DummyTaskDefinition } from '../dummy-task-definition';
 import { EcsService, PropagateTags } from '../ecs-service';
 
 describe('EcsService', () => {
+  const app = new cdk.App();
+
   describe('with default props', () => {
-    const app = new cdk.App();
     const stack = new cdk.Stack(app, 'MyStackWithDefaults');
     const cluster = new ecs.Cluster(stack, 'Cluster');
     const prodTargetGroup = new elb.ApplicationTargetGroup(stack, 'ProdTargetGroup', { vpc: cluster.vpc });
@@ -25,18 +24,17 @@ describe('EcsService', () => {
       taskDefinition,
     });
 
-    const template = Template.fromStack(stack);
-
     test('Creates a BlueGreenService custom resource', () => {
-      template.hasResourceProperties('Custom::BlueGreenService', {
-        ServiceName: 'My Service',
-        LaunchType: 'FARGATE',
-      });
+      expectCDK(stack).to(
+        haveResource('Custom::BlueGreenService', {
+          ServiceName: 'My Service',
+          LaunchType: 'FARGATE',
+        }),
+      );
     });
   });
 
   describe('with tag propagation', () => {
-    const app = new cdk.App();
     const stack = new cdk.Stack(app, 'MyStackWithTagPropagation');
     const cluster = new ecs.Cluster(stack, 'Cluster');
     const prodTargetGroup = new elb.ApplicationTargetGroup(stack, 'ProdTargetGroup', { vpc: cluster.vpc });
@@ -52,19 +50,18 @@ describe('EcsService', () => {
       propagateTags: PropagateTags.SERVICE,
     });
 
-    const template = Template.fromStack(stack);
-
     test('enables tag propagation', () => {
-      template.hasResourceProperties('Custom::BlueGreenService', {
-        ServiceName: 'My Service',
-        LaunchType: 'FARGATE',
-        PropagateTags: 'SERVICE',
-      });
+      expectCDK(stack).to(
+        haveResource('Custom::BlueGreenService', {
+          ServiceName: 'My Service',
+          LaunchType: 'FARGATE',
+          PropagateTags: 'SERVICE',
+        }),
+      );
     });
   });
 
   describe('with tags', () => {
-    const app = new cdk.App();
     const stack = new cdk.Stack(app, 'MyStackWithTags');
     const cluster = new ecs.Cluster(stack, 'Cluster');
     const prodTargetGroup = new elb.ApplicationTargetGroup(stack, 'ProdTargetGroup', { vpc: cluster.vpc });
@@ -81,17 +78,17 @@ describe('EcsService', () => {
       taskDefinition,
     });
 
-    const template = Template.fromStack(stack);
-
     test('adds Tags to the BlueGreenService', () => {
-      template.hasResourceProperties('Custom::BlueGreenService', {
-        Tags: [
-          {
-            Key: 'Foo',
-            Value: 'Bar',
-          },
-        ],
-      });
+      expectCDK(stack).to(
+        haveResource('Custom::BlueGreenService', {
+          Tags: [
+            {
+              Key: 'Foo',
+              Value: 'Bar',
+            },
+          ],
+        }),
+      );
     });
   });
 });
